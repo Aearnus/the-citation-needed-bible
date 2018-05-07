dda089775dae98aab9ecd5f5cc87080ba000a5a9
Tyler L <tslimkemann42@gmail.com>
2018-05-07 01:26:48 -0700

    startup correctly

--- Main.rb
+++ Main.rb
@@ -4,16 +4,16 @@ require_relative "BibleTwitter.rb"
 require "pp"
 
 if __FILE__ == $0
-    puts "Beginning query thread, every 600 seconds..."
+    bibleTwitter = BibleTwitter.new()
+    puts "Recieved application auth..."
+    puts "Beginning query thread, every 20 minutes..."
     Thread.new do
         loop do
             puts "Updating database..."
-            bibleTwitter = BibleTwitter.new()
-            puts "Recieved application auth..."
             bibleTwitter.updateDatabase()
-            sleep 600
+            sleep 1200
         end
     end
     puts "Beginning server..."
-    puts `./site/Host.rb`
+    puts `cd site; ./Host.rb`
 end
